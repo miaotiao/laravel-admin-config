9930a9a7274a9f361c9690551d19343e37700257
miaotiao <miaotiao@users.noreply.github.com>
2021-03-04 13:14:58 +0000

    Apply fixes from StyleCI

--- src/ConfigServiceProvider.php
+++ src/ConfigServiceProvider.php
@@ -7,14 +7,14 @@ use Illuminate\Support\ServiceProvider;
 class ConfigServiceProvider extends ServiceProvider
 {
     /**
-     * 解决数据库不存在的时候报错
+     * 解决数据库不存在的时候报错.
      */
     public function boot()
     {
         try {
             if ($this->app->runningInConsole()) {
-                $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-                $this->publishes([__DIR__ . '/../database/seeds' => database_path('seeds')]);
+                $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+                $this->publishes([__DIR__.'/../database/seeds' => database_path('seeds')]);
             } else {
                 Config::load();
             }
